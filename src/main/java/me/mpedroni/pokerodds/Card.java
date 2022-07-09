fe26c64a5aaa5d63f0db7653bacb2f770c4666af
Matheus Pedroni <mpedroni77@gmail.com>
2022-07-09 01:01:34 -0300

    Refactor Card class and CardCreation test

--- src/main/java/me/mpedroni/pokerodds/Card.java
+++ src/main/java/me/mpedroni/pokerodds/Card.java
@@ -1,14 +1,11 @@
 package me.mpedroni.pokerodds;
 
 public class Card {
-    private CardNumber number;
+    public CardNumber number;
     public CardSuit suit;
+
     public Card(CardNumber number, CardSuit suit) {
         this.number = number;
         this.suit = suit;
-    }
-
-    public CardNumber getNumber() {
-        return number;
     }
 }
--- src/test/java/PokerOddsTest.java
+++ src/test/java/PokerOddsTest.java
@@ -1,15 +1,12 @@
-import me.mpedroni.pokerodds.Card;
-import me.mpedroni.pokerodds.CardNumber;
-import me.mpedroni.pokerodds.CardSuit;
+import me.mpedroni.pokerodds.*;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PokerOddsTest {
     @Test
     void TestCardNumberCreation() {
         Card card = new Card(CardNumber.ACE, CardSuit.SPADES);
-        assertEquals(card.getNumber(), CardNumber.ACE);
+        assertEquals(card.number, CardNumber.ACE);
         assertEquals(card.suit, CardSuit.SPADES);
     }
 }
