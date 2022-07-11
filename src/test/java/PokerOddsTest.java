635c428b69d26e4eac65d07522775e2aeaf6a6e4
Matheus Pedroni <mpedroni77@gmail.com>
2022-07-11 18:34:10 -0300

    Rename test

--- src/test/java/PokerOddsTest.java
+++ src/test/java/PokerOddsTest.java
@@ -1,10 +1,13 @@
-import me.mpedroni.pokerodds.*;
+import me.mpedroni.pokerodds.Card;
+import me.mpedroni.pokerodds.CardNumber;
+import me.mpedroni.pokerodds.CardSuit;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PokerOddsTest {
     @Test
-    void TestCardNumberCreation() {
+    void TestCardCreation() {
         Card card = new Card(CardNumber.ACE, CardSuit.SPADES);
         assertEquals(card.number, CardNumber.ACE);
         assertEquals(card.suit, CardSuit.SPADES);
