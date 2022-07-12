13d6fae654eefacf75a859a5798203bdd9546128
Matheus Pedroni <mpedroni77@gmail.com>
2022-07-11 22:15:09 -0300

    Create Table class

--- src/main/java/me/mpedroni/pokerodds/Table.java
+++ src/main/java/me/mpedroni/pokerodds/Table.java
@@ -4,19 +4,18 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Player {
+public class Table {
     public Set<Card> cards;
 
-    public Player(Card... cards) throws Exception {
-        if (cards.length > 2) {
-            throw new Exception("Players can have up to 2 cards");
-        }
+    public Table(Card... cards) throws Exception {
+        if (cards.length > 5)
+            throw new Exception("Tables can have up to 5 cards");
+
         this.cards = new HashSet<>();
         Collections.addAll(this.cards, cards);
     }
-    
 
     public boolean has(Card card) {
-        return cards.contains(card);
+        return this.cards.contains(card);
     }
 }
--- src/test/java/PokerOddsTest.java
+++ src/test/java/PokerOddsTest.java
@@ -1,14 +1,11 @@
-import me.mpedroni.pokerodds.Card;
-import me.mpedroni.pokerodds.CardNumber;
-import me.mpedroni.pokerodds.CardSuit;
-import me.mpedroni.pokerodds.Player;
+import me.mpedroni.pokerodds.*;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PokerOddsTest {
     @Test
-    void TestCardCreate() {
+    void TestCardWithNumberAndSuit() {
         Card card = new Card(CardNumber.ACE, CardSuit.SPADES);
         assertEquals(card.number, CardNumber.ACE);
         assertEquals(card.suit, CardSuit.SPADES);
@@ -26,7 +23,7 @@ public class PokerOddsTest {
     }
 
     @Test
-    void TestPlayerCreate() throws Exception {
+    void TestPlayerCanHaveUpTwoCards() throws Exception {
         Card aceOfDiamonds = new Card(CardNumber.ACE, CardSuit.DIAMONDS);
         Card aceOfSpades = new Card(CardNumber.ACE, CardSuit.SPADES);
         Player player = new Player(aceOfDiamonds, aceOfSpades);
@@ -35,17 +32,53 @@ public class PokerOddsTest {
     }
 
     @Test
-    void TestPlayerCreateWithZeroCards() {
+    void TestPlayerCanHaveNoCards() throws Exception {
         Player player = new Player();
         assertTrue(player.cards.isEmpty());
     }
 
     @Test
-    void TestPlayerCreateWithMoreThanCardsLimit() {
+    void TestPlayerCannotHaveMoreThanTwoCards() {
         Card aceOfDiamonds = new Card(CardNumber.ACE, CardSuit.DIAMONDS);
         Card aceOfSpades = new Card(CardNumber.ACE, CardSuit.SPADES);
         Card aceOfHearts = new Card(CardNumber.ACE, CardSuit.HEARTS);
         Exception exception = assertThrows(Exception.class, () -> new Player(aceOfDiamonds, aceOfSpades, aceOfHearts));
         assertEquals(exception.getMessage(), "Players can have up to 2 cards");
+    }
+
+    @Test
+    void TestTableCanHaveUpToFiveCards() throws Exception {
+        Card aceOfSpades = new Card(CardNumber.ACE, CardSuit.SPADES);
+        Card aceOfHearts = new Card(CardNumber.ACE, CardSuit.HEARTS);
+        Card aceOfDiamonds = new Card(CardNumber.ACE, CardSuit.DIAMONDS);
+        Card aceOfClubs = new Card(CardNumber.ACE, CardSuit.CLUBS);
+        Card kingOfSpades = new Card(CardNumber.KING, CardSuit.SPADES);
+
+        Table table = new Table(aceOfSpades, aceOfHearts, aceOfDiamonds, aceOfClubs, kingOfSpades);
+
+        assertTrue(table.has(aceOfSpades));
+        assertTrue(table.has(aceOfHearts));
+        assertTrue(table.has(aceOfDiamonds));
+        assertTrue(table.has(aceOfClubs));
+        assertTrue(table.has(kingOfSpades));
+    }
+
+    @Test
+    void TestTableCannotHaveMoreThanFiveCards() {
+        Card aceOfSpades = new Card(CardNumber.ACE, CardSuit.SPADES);
+        Card aceOfHearts = new Card(CardNumber.ACE, CardSuit.HEARTS);
+        Card aceOfDiamonds = new Card(CardNumber.ACE, CardSuit.DIAMONDS);
+        Card aceOfClubs = new Card(CardNumber.ACE, CardSuit.CLUBS);
+        Card kingOfSpades = new Card(CardNumber.KING, CardSuit.SPADES);
+        Card kingOfDiamonds = new Card(CardNumber.KING, CardSuit.DIAMONDS);
+
+        Exception exception = assertThrows(Exception.class, () -> new Table(aceOfSpades, aceOfHearts, aceOfDiamonds, aceOfClubs, kingOfSpades, kingOfDiamonds));
+        assertEquals(exception.getMessage(), "Tables can have up to 5 cards");
+    }
+
+    @Test
+    void TestTableCanHaveZeroCards() throws Exception {
+        Table table = new Table();
+        assertTrue(table.cards.isEmpty());
     }
 }
